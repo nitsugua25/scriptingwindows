b0e001ca109dbfb90c4b005da72d19d9125a8921
sAkAD0 <ugodegueldre03@gmail.com>
2024-11-25 15:51:14 +0100

    feat(main05092003): creating ad user script

---
+++ main05092003..ps1
@@ -0,0 +1,28 @@
+Import-Module ImportExcel
+$Path = "./Employes.xlsx"
+$outputFile = "./Doublons.xlsx"
+
+$Users = $Path
+
+foreach ($User in $Users) {
+    $User = $User.SamAccountName
+
+    if (Get-ADUser -Filter {SamAccountName -eq $User}) {
+        Write-Host "Exporting data to Excel..."
+        $Data | Export-Excel -Path $outputFile -WorksheetName 'Doublons' -NoClobber:$false
+    }
+    else {
+        userProps = @{
+            Name = $User.Nom
+            Surname = $User.Prenom
+            EmailAddress = $User.Login
+            Description = $User.description
+            Departement = $User.Departement
+            Office = $User.Bureau
+            OfficeNumber = $User.Numero_interne
+            AccountPassword = $User.Password
+        }
+        New-ADUser @userProps
+        Write-Host "User $User created"
+    }
+}
