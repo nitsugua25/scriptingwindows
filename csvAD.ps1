e4ba2e9fd8386bc2207fccb30151b42fb0baea50
Torreip <80983183+Torreip@users.noreply.github.com>
2024-11-27 17:35:11 +0100

    automatic stuff should be done putting the hundreds of permision in soonTM

--- csvAD.ps1
+++ csvAD.ps1
@@ -19,7 +19,120 @@ function New-ADGG {
         }
     }
     catch {
-        Write-Error "Erreur lors de la création de l'OU $OUName : $_"
+        Write-Error "Erreur lors de la création du groupe $GName : $_"
+    }
+}
+
+function New-ADGL {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$GName,
+        [Parameter(Mandatory=$true)]
+        [string]$BaseDN
+    )
+    try {
+        $GN = Get-ADGroup -Filter { Name -eq $GName } -SearchBase $BaseDN
+        if($null -eq $GN) {
+            Write-Host "Création du groupe $GName"
+            New-ADGroup -Name "$GName" -Path "$BaseDN" -GroupCategory Security -GroupScope Local
+        }
+        else {
+            Write-Host "Le groupe $GName existe déja."
+        }
+    }
+    catch {
+        Write-Error "Erreur lors de la création du groupe $GName : $_"
+    }
+}
+
+function New-GPO-Share {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$basePath
+    )
+    New-Item -Path "$basePath" -Name "gpo$" -ItemType "directory"
+    New-Item -Path "$basePath" -Name "user-doccuments$" -ItemType "directory"
+    New-SmbShare -Name "gpo$" -Path "$basePath\gpo$" -ReadAccess "BELGIQUE\Domain Users" -FullAccess "BELGIQUE\Domain Admins"
+    New-SmbShare -Name "user-doccuments$" -Path "$basePath\user-doccuments$" -ChangeAccess "BELGIQUE\Domain Users" -FullAccess "BELGIQUE\Domain Admins"
+}
+
+function Set-AGDLP-and-Main-Share {
+    param(
+        [Parameter(Mandatory=$true)]
+        [hashtable]$repList,
+        [Parameter(Mandatory=$true)]
+        [string]$BaseDN,
+        [Parameter(Mandatory=$true)]
+        [string]$basePath
+    )
+    $permTable = @{
+        "GL-direction-rw" = @("GG-direction")
+        "GL-ressources humaine-r" = @()
+        "GL-ressources humaine-rw" = @()
+        "GL-gestion du personnel-r" = @()
+        "GL-gestion du personnel-rw" = @()
+        "GL-recrutement-r" = @()
+        "GL-recrutement-rw" = @()
+        "GL-r&d-r" = @()
+        "GL-r&d-rw" = @()
+        "GL-recherche-r" = @()
+        "GL-recherche-rw" = @()
+        "GL-testing-r" = @()
+        "GL-testing-rw" = @()
+        "GL-marketing-r" = @()
+        "GL-marketing-rw" = @()
+        "GL-site 1-r" = @()
+        "GL-site 1-rw" = @()
+        "GL-site 2-r" = @()
+        "GL-site 2-rw" = @()
+        "GL-site 3-r" = @()
+        "GL-site 3-rw" = @()
+        "GL-site 4-r" = @()
+        "GL-site 4-rw" = @()
+        "GL-informatique-r" = @()
+        "GL-informatique-rw" = @()
+        "GL-systemes-r" = @()
+        "GL-systemes-rw" = @()
+        "GL-developpement-r" = @()
+        "GL-developpement-rw" = @()
+        "GL-hotline-r" = @()
+        "GL-hotline-rw" = @()
+        "GL-finances-r" = @()
+        "GL-finances-rw" = @()
+        "GL-comptabilite-r" = @()
+        "GL-comptabilite-rw" = @()
+        "GL-investissements-r" = @()
+        "GL-investissements-rw" = @()
+        "GL-technique-r" = @()
+        "GL-technique-rw" = @()
+        "GL-techniciens-r" = @()
+        "GL-techniciens-rw" = @()
+        "GL-achat-r" = @()
+        "GL-achat-rw" = @()
+        "GL-commerciaux-r" = @()
+        "GL-commerciaux-rw" = @()
+        "GL-sedentaires-r" = @()
+        "GL-sedentaires-rw" = @()
+        "GL-technico-r" = @()
+        "GL-technico-rw" = @()
+    }
+
+    New-Item -Path "$basePath" -Name "share$" -ItemType "directory"
+    $basePath += "\share$\"
+    New-SmbShare -Name "share$" -Path "$basePath" -ChangeAccess "BELGIQUE\Domain Users" -FullAccess "BELGIQUE\Domain Admins"
+
+    foreach($key in $repList.Keys) {
+        New-Item -Path "$basePath" -Name "$key" -ItemType "directory"
+        foreach($depPath in $key){
+            New-Item -Path "$basePath$key\" -Name "$depPath" -ItemType "directory"
+        }
+    }
+
+    foreach($key in $permTable.Keys) {
+        New-ADGL -GName "$key" -BaseDN "$baseDN"
+        foreach($perm in $permTable["$key"]) {
+            Add-ADGroupMember -Identity "$key" -Members "$perm"
+        }
     }
 }
 
@@ -81,9 +194,15 @@ function New-RandomPassword {
 
 # Programme principal
 try {
-    New-ADOU -OUName "Groupes" -BaseDN "DC=belgique,DC=lan"
-    New-ADOU -OUName "Groupes Globaux" -BaseDN "OU=Groupes,DC=belgique,DC=lan"
-    New-ADOU -OUName "Groupes Locaux" -BaseDN "OU=Groupes,DC=belgique,DC=lan"
+
+    $rootdc = "DC=belgique,DC=lan"
+
+    New-ADOU -OUName "Groupes" -BaseDN "$rootdc"
+    New-ADOU -OUName "Groupes Globaux" -BaseDN "OU=Groupes,$rootdc"
+    New-ADOU -OUName "Groupes Locaux" -BaseDN "OU=Groupes,$rootdc"
+    New-ADOU -OUName "ordinateurs" -BaseDN "$rootdc"
+
+    redircmp "OU=ordinateurs,$rootdc" #Redirect Old Computers to New target
 
     Write-Host "Lecture du fichier CSV..."
     $Users = Import-Csv -Path ".\output.csv" -Encoding UTF8
@@ -91,6 +210,8 @@ try {
     $AvailableUPN = @()
     $userList = @()
     $Blacklist = @()
+    $repList = @{}
+
 
     foreach ($User in $Users) {
         if ("$($User.Nom) $($User.Prenom)" -notin $userList) {
@@ -117,25 +238,33 @@ try {
                 if ($DepList -notcontains $Departement[1]) {
                     $DepList += $Departement[1]
                     Write-Host ("Création de l'OU " + $Departement[1])
-                    New-ADOU -OUName $Departement[1] -BaseDN "DC=belgique,DC=lan"
-                    New-ADGG -gname "GG-responsable-$($Departement[1])" -BaseDN "OU=Groupes Globaux,OU=Groupes,DC=belgique,DC=lan"
+                    New-ADOU -OUName $Departement[1] -BaseDN "$rootdc"
+                    New-ADGG -gname "GG-responsable-$($Departement[1])" -BaseDN "OU=Groupes Globaux,OU=Groupes,$rootdc"
+                    New-ADGL -gname "GL-$($Departement[1])-r" -BaseDN "OU=Groupes Locaux,OU=Groupes,$rootdc"
+                    New-ADGL -gname "GL-$($Departement[1])-rw" -BaseDN "OU=Groupes Locaux,OU=Groupes,$rootdc"
+                    $repList.Add($Departement[1], @() )
                 }
 
                 if ($DepList -notcontains $Departement[0]) {
                     $DepList += $Departement[0]
-                    $baseDN = "OU=" + $Departement[1] + ",DC=belgique,DC=lan".Trim()
+                    $baseDN = "OU=" + $Departement[1] + ",$rootdc".Trim()
                     New-ADOU -OUName $Departement[0].Trim() -BaseDN $BaseDN
-                    New-ADGG -gname "GG-$($Departement[0])" -BaseDN "OU=Groupes Globaux,OU=Groupes,DC=belgique,DC=lan"
+                    New-ADGG -gname "GG-$($Departement[0])" -BaseDN "OU=Groupes Globaux,OU=Groupes,$rootdc"
+                    New-ADGL -gname "GL-$($Departement[0])-r" -BaseDN "OU=Groupes Locaux,OU=Groupes,$rootdc"
+                    New-ADGL -gname "GL-$($Departement[0])-rw" -BaseDN "OU=Groupes Locaux,OU=Groupes,$rootdc"
                     $depHead = $true
+                    $repList[$Departement[1]] += $Departement[0]
                 }
-                $ParsedDN = ("OU=" + $Departement[0] + ",OU=" + $Departement[1] + ",DC=belgique,DC=lan").Trim()
+                $ParsedDN = ("OU=" + $Departement[0] + ",OU=" + $Departement[1] + ",$rootdc").Trim()
             } else {
                 if ($DepList -notcontains $Departement[0]) {
                     $DepList += $Departement[0]
-                    New-ADOU -OUName $Departement[0] -BaseDN "DC=belgique,DC=lan"
-                    New-ADGG -gname "GG-$($Departement[0])" -BaseDN "OU=Groupes Globaux,OU=Groupes,DC=belgique,DC=lan"
+                    New-ADOU -OUName $Departement[0] -BaseDN "$rootdc"
+                    New-ADGG -gname "GG-$($Departement[0])" -BaseDN "OU=Groupes Globaux,OU=Groupes,$rootdc"
+                    New-ADGL -gname "GL-$($Departement[0])-r" -BaseDN "OU=Groupes Locaux,OU=Groupes,$rootdc"
+                    New-ADGL -gname "GL-$($Departement[0])-rw" -BaseDN "OU=Groupes Locaux,OU=Groupes,$rootdc"
                 }
-                $parsedDN = ("OU=" + $Departement[0] + ",DC=belgique,DC=lan").Trim()
+                $parsedDN = ("OU=" + $Departement[0] + ",$rootdc").Trim()
             }
 
             if($depHead -eq $false) {
@@ -222,6 +351,7 @@ try {
             Write-Error "Erreur lors de la création de l'utilisateur $baseUPN : $_"
         }
     }
+
 } catch {
     Write-Error "Erreur générale : $_"
     exit 1
